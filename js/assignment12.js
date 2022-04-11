b8fea30a056ee52a289744d1c270528d257b2a5c
Jessica Lieu <jeli8022@colorado.edu>
2022-04-10 23:06:18 -0600

    new

--- js/assignment12.js
+++ js/assignment12.js
@@ -23,13 +23,3 @@ function displayQuote(quote) {
   const quoteText = document.querySelector('#js-quote-text');
   quoteText.textContent = quote;
 }
-
-function buttonClicked() {
-  document.getElementById('button-1').classList.toggle('is-pink');
-}
-
-function windowLoaded() {
-  document.getElementById('button-1').addEventListener('click', buttonClicked);
-}
-
-window.onload = windowLoaded;
