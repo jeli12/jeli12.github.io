9828e1ddfca5417310466d571aa128b0e1549c50
Jessica Lieu <jeli8022@colorado.edu>
2022-04-14 11:36:29 -0600

    new

--- assignment13.js
+++ assignment13.js
@@ -29,4 +29,9 @@ function result() {
 
   story.textContent = ;
   story.style.visibility = 'visible';
+
+  const newStory = storyText
+  const xItem = randomValueFromArray
+  const yItem = randomValueFromArray
+  const zItem = randomValueFromArray
 }
